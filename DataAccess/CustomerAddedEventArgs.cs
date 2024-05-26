bd72220db133bfab62855dbc300c831c5f0296e8
Secret787 <117649934+Secret787@users.noreply.github.com>
2024-05-26 23:24:54 +0700

    Добавьте файлы проекта.

---
+++ DataAccess/CustomerAddedEventArgs.cs
@@ -0,0 +1,19 @@
+﻿using CreditCalculator.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CreditCalculator.DataAccess
+{
+    public class CustomerAddedEventArgs : EventArgs
+    {
+        public CustomerAddedEventArgs(Customer newCustomer)
+        {
+            this.NewCustomer = newCustomer;
+        }
+
+        public Customer NewCustomer { get; private set; }
+    }
+}
---
+++ ViewModel/MainVM.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CreditCalculator.Model;
+
+namespace CreditCalculator.ViewModel
+{
+
+    public class MainVM : INotifyPropertyChanged
+    {
+        private int _number1;
+        public int Number1
+        {
+            get { return _number1; }
+            set
+            {
+                _number1 = value;
+                OnPropertyChanged("Number3"); // уведомление View о том, что изменилась сумма
+            }
+        }
+
+        private int _number2;
+        public int Number2
+        {
+            get { return _number2; }
+            set { _number2 = value; OnPropertyChanged("Number3"); }
+        }
+        public int Number3
+        {
+            get
+            {
+                return MathFuncs.GetSumOf(_number1, _number2);
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+
+
+}
